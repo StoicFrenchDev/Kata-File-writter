671d0e422348d556de1df0dac029dbeaa664662d
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2023-12-07 15:57:09 +0100

    Created the kata

---
+++ kata.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Faire un programme qui écrit dans un fichier nommé "mantra.txt".
+ * Il devra écrire dans ce fichier "PHP est le meilleur language du monde!!" sur une ligne 10000 fois.
+ */
