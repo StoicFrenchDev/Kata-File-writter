6bfaaf794f69d5de0037ac29330488a003c88f50
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2023-12-13 09:41:24 +0100

    Correction added

--- kata.php
+++ kata.php
@@ -4,3 +4,10 @@
  * Faire un programme qui écrit dans un fichier nommé "mantra.txt".
  * Il devra écrire dans ce fichier "PHP est le meilleur langage du monde!!" sur une ligne 10000 fois.
  */
+$handler = fopen('mantra.txt', 'w');
+
+for ($i=0; $i < 10000; $i++) { 
+   fwrite($handler, 'PHP est le meilleur langage du monde!!' . PHP_EOL);
+}
+
+fclose($handler);
