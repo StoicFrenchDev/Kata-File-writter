b2c4acad011f585263334012b4e98df2d8ddf64d
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2023-12-13 09:04:11 +0100

    fixed a typo

--- kata.php
+++ kata.php
@@ -2,5 +2,5 @@
 
 /**
  * Faire un programme qui écrit dans un fichier nommé "mantra.txt".
- * Il devra écrire dans ce fichier "PHP est le meilleur language du monde!!" sur une ligne 10000 fois.
+ * Il devra écrire dans ce fichier "PHP est le meilleur langage du monde!!" sur une ligne 10000 fois.
  */
